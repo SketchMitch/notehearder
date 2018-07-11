dfd2e93154163cd2de08358132741d00173d3dcd
Mitchell Sayre <sketch0306@gmail.com>
2018-07-10 17:39:58 -0700

    Handle mouse events in sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,34 +4,66 @@ import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
-const Sidebar = () => {
-    return(
-     <div 
-     className="sidebar" 
-     style={styles.sidebar}
-     >
-        <div 
-            className="logo"
-            style={styles.logo}
-        >
-            <img 
-                src={quill} 
-                alt="Noteherder" 
-                style={styles.LogoImg}
-            />
-        </div>
+class Sidebar extends React.Component {
+    state = {
+        newIconHovered: false,
+    }
 
-        <a href="/notes">
-        <img src={newHover} alt="New note" />
-        <img src={newIcon} alt="New Note" />
-        </a>
-        <div className="SignOut">
-            <button>
-              <i className="fa fa-sign-out"></i>
-            </button>
-          </div> 
-        </div> 
-    )
+    handleMouseEnter() {
+        this.setState({ newIconHovered: true })
+    }
+
+    handleMouseLeave() {
+        this.setState({ newIconHovered: false })
+    }
+    render() {
+        return (
+                <nav
+                    className="sidebar"
+                    style={styles.sidebar}
+                >
+                    <div
+                        className="logo"
+                        style={styles.logo}
+                    >
+                        <img
+                            src={quill}
+                            alt="Noteherder"
+                            style={styles.LogoImg}
+                        />
+                    </div>
+
+                    <a
+                        className="new-note"
+                        href="/notes"
+                        style={styles.newNote}
+                        onMouseEnter={() => this.handleMouseEnter()}
+                        onMouseLeave={() => this.handleMouseLeave()}
+                    >
+                        <img
+                            src={newHover}
+                            alt="New note"
+                            style={styles.newNoteImg}
+                        />
+                        <img
+                            className="outline"
+                            src={newIcon}
+                            alt="New note"
+                            style={{
+                                ...styles.newNoteImg,
+                                opacity: this.state.newIconHovered ? 0 : 1
+                            }}
+                        />
+                    </a>
+
+                    <div className="SignOut">
+                        <button>
+                            <i className="fa fa-sign-out"></i>
+                        </button>
+                    </div>
+                </nav>
+        )
+    }
 }
 
 const styles = {
@@ -51,6 +83,33 @@ const styles = {
     LogoImg: {
         width: '3rem',
         paddingLeft: '0.4rem',
-    }
+    },
+    newNote: {
+        marginTop: "2rem",
+        position: "relative",
+        width: "4rem",
+    },
+    newNoteImg: {
+        position: 'absolute',
+        left: '0',
+        width: '100%',
+        transition: 'opacity 0.25s ease-in-out',
+    },
+    signOut: {
+        position: 'absolute',
+        bottom: '1rem',
+    },
+    button: {
+        backgroundColor: 'transparent',
+        border: '0',
+        color: '#008df8',
+        cursor: 'pointer',
+        outline: 'none',
+    },
+    buttonIcon: {
+        fontSize: '2rem',
+    },
+
+
 }
 export default Sidebar
