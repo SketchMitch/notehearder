f7e3b92bd386853759e88031467fd6a21b34b8da
Mitchell Sayre <sketch0306@gmail.com>
2018-07-15 14:51:27 -0700

    Add a key to each Note

--- src/NoteList.js
+++ src/NoteList.js
@@ -27,7 +27,7 @@ const NoteList = () => {
                 <ul id="notes">
                     <a className="active">
                         {
-                            notes.map(note => <Note note={note} />)}
+                            notes.map(note => <Note key={note.id} note={note} />)}
                         }
                     </a>
                 </ul>
