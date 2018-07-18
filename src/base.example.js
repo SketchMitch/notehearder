1b04e68f48dea9e2e4fda382555caaa5c5662925
Mitchell Sayre <sketch0306@gmail.com>
2018-07-17 17:04:12 -0700

    Update re-base config example

--- src/base.example.js
+++ src/base.example.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
  import database from 'firebase/database'
+ import 'firebase/auth'
  import Rebase from 're-base'
 
 
@@ -12,7 +13,12 @@ import firebase from 'firebase/app'
     storageBucket: "YOUR STORAGE BUCKET",
     messagingSenderId: "YOUR MESSAGING SENDER ID"
   }
+ 
   const app = firebase.initializeApp(config)
   const db = firebase.database(app)
+
+  export const githubProvider = new firebase.auth.GithubAuthProvider()
+  export const auth = app.auth()
+ 
 
   export default Rebase.createClass(db)
